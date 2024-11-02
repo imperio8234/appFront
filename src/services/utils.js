7221b5ed07cf436dd1e151d791b4e90af7442d52
Jhon Anderson Garcia <garciacalvojhonanderson@gmail.com>
2024-11-02 18:24:49 -0500

    proyecto front

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_URL = "https://rickandmortyapi.com/api";
-const API_BACK = "http://localhost:4000/api"
+const API_BACK = "http://157.245.246.81:4000/api"
 
 
 export const apiRick = axios.create({
